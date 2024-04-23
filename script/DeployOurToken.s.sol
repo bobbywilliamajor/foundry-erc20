c19fb4d756b63048c1fa413518febddb1f8c8605
bobbywilliamajor <bobbywilliamajor@gmail.com>
2024-04-23 05:09:07 +0200

    first commit

---
+++ script/DeployOurToken.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+contract DeployOurToken is Script {
+    uint256 public constant INITIAL_SUPPLY = 1000 ether;
+    OurToken token;
+
+    function run() external returns (OurToken) {
+        vm.startBroadcast();
+        token = new OurToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+        return token;
+    }
+}
